2d3621135e6626ff4e6817deb59cd20bb915178e
Clayton Smith <argilo@gmail.com>
2022-11-23 15:13:26 -0500

    Start work on generation script

--- sigid/gen.sh
+++ sigid/gen.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+SAMP_RATE=960000
+
 python3 gen_flags.py
 
+grcc sig1.grc
 convert \
   -background White \
   -gravity Center \
@@ -13,12 +16,10 @@ convert \
   -extent x2300 \
   -background Black \
   -extent 256x \
-  paint.png
-
-grcc paint_flag.grc
-python3 paint_flag.py
+  flag1.png
+python3 sig1.py --samp-rate=${SAMP_RATE} --offset=0
 
 sox flag8.wav -r 8000 -t raw - | ~/git/m17-cxx-demod/build/apps/m17-mod -S VE3IRR -b > flag8.bin
 
-PYTHONPATH=.:$PYTHONPATH grcc multi_tx.grc
-python3 multi_tx.py
+#PYTHONPATH=.:$PYTHONPATH grcc multi_tx.grc
+#python3 multi_tx.py
