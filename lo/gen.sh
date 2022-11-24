51ac312228e8c0f01830177dcb7225054a02954a
Clayton Smith <argilo@gmail.com>
2022-11-24 11:43:28 -0500

    Make sample rate and offsets configurable

--- lo/gen.sh
+++ lo/gen.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+SAMP_RATE=5760000
+
 python3 gen_flags.py
 
 grcc lo1.grc
-python3 lo1.py
+python3 lo1.py --samp-rate=${SAMP_RATE} --offset=1000000
 
 grcc lo2.grc
-python3 lo2.py
+python3 lo2.py --samp-rate=${SAMP_RATE} --offset=-1000000
